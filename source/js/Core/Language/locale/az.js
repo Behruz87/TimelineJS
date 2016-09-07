74d72ffe77fdb54debbe618405a2b9e9689a67a9
Behruz87 <behruz6988@yahoo.com>
2016-09-07 18:12:27 +0400

    Create az.js
    Adding Azerbaijani language file(az.js) to TimelineJS

---
+++ source/js/Core/Language/locale/az.js
@@ -0,0 +1,38 @@
+/* Azerbaijani LANGUAGE 
+================================================== */
+if(typeof VMM != 'undefined') {
+	VMM.Language = {
+		lang: "az",
+		api: {
+			wikipedia: "az"
+		},
+		date: {
+			month: ["Yanvar", "Fevral", "Mart", "Aprel", "May", "İyun", "İyul", "Avqust", "Sentyabr", "Oktyabr", "Noyabr", "Dekabr"],
+			month_abbr: ["Yan.", "Fev.", "Mart", "Aprel", "May", "İyun", "İyul", "Avq.", "Sent.", "Okt.", "Noy.", "Dek."],
+			day: ["Bazar","Bazar ertəsi", "Çərşənbə axşamı", "Çərşənbə", "Cümə axşamı", "Cümə", "Şənbə"],
+			day_abbr: ["Baz.","B.e.", "Ç.a.", "Çər.", "C.a.", "Cüm.", "Şnb."]
+		}, 
+		dateformats: {
+			year: "yyyy",
+			month_short: "mmm",
+			month: "mmmm yyyy",
+			full_short: "mmm d",
+			full: "mmmm d',' yyyy",
+			time_short: "h:MM:ss TT",
+			time_no_seconds_short: "h:MM TT",
+			time_no_seconds_small_date: "h:MM TT'<br/><small>'mmmm d',' yyyy'</small>'",
+			full_long: "mmm d',' yyyy 'at' h:MM TT",
+			full_long_small_date: "h:MM TT'<br/><small>mmm d',' yyyy'</small>'"
+		},
+		messages: {
+			loading_timeline: "Zaman şəridi yüklənir... ",
+			return_to_title: "Başlığa qayıt",
+			expand_timeline: "Zaman şəridini genişləndir",
+			contract_timeline: "Zaman şəridi müqaviləsi",
+			wikipedia: "pulsuz ensiklopediya olan wikipediyadan",
+			loading_content: "Kontent Yüklənir",
+			loading: "Yüklənir",
+			swipe_nav: "Baxmaq üçün sürüşdürün"
+		}
+	}
+}
